8b90e27e83c09522cf9ca29f9b60e42d60b404b0
lucita287@gmail.com <lucita287@gmail.com@75f326e9-409e-c376-3f9a-58dcc74cf6c3>
2015-02-22 23:12:01 +0000

    Se Agrega toda la estructura del proyecto

---
+++ trunk/componentes/matic-as02/src/main/java/gt/usac/ing/tic/view/CompraBean.java
@@ -0,0 +1,65 @@
+package gt.usac.ing.tic.view;
+
+import gt.usac.ing.tic.modelo.dto.TicUserDto;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ComponentSystemEvent;
+
+import org.primefaces.context.RequestContext;
+
+@ManagedBean(name = "compraBean")
+@ViewScoped
+public class CompraBean  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private TicUserDto loginUser=null;
+	
+	@PostConstruct
+    public void init() {
+		
+    }
+	
+	 public void iniciarSession(ComponentSystemEvent event){
+		 
+		  	
+	 }
+	 
+	 public void loginUser(ActionEvent actionEvent) {
+		 Redirect("pagosOnline.xhtml");
+	}
+	 
+	 public void loginClose(ActionEvent actionEvent) {
+		 Redirect("../index.xhtml");
+	}
+	 
+	 
+	 public void pagoServicio(ActionEvent actionEvent) {
+		 Redirect("resultado.xhtml");
+	}
+
+	public TicUserDto getLoginUser() {
+		return loginUser;
+	}
+
+	public void setLoginUser(TicUserDto loginUser) {
+		this.loginUser = loginUser;
+	}
+	
+	public String Redirect(String path){
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(path);
+           
+        } catch (IOException ex) {
+             
+        }
+          return "";
+    }
+	 
+}
