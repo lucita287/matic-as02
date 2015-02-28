6d0750b0b978f4ed61cab2c23590a40cc37b5b90
lucita287@gmail.com <lucita287@gmail.com@75f326e9-409e-c376-3f9a-58dcc74cf6c3>
2015-02-28 10:41:12 +0000

    agregando mas escenarios de prueba

---
+++ trunk/primer_proyecto/matic-as02/src/test/java/gt/usac/ing/tic/BACScenarioSteps.java
@@ -0,0 +1,134 @@
+package gt.usac.ing.tic;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import gt.usac.ing.tic.modelo.dto.TicCuentaDto;
+import gt.usac.ing.tic.modelo.dto.TicFacturaDto;
+import gt.usac.ing.tic.modelo.dto.TicUserDto;
+import gt.usac.ing.tic.srv.GeneralSrv;
+import gt.usac.ing.tic.srv.impl.GeneralSrvImpl;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+public class BACScenarioSteps {
+
+	TicCuentaDto v_Cuenta=null;
+	TicFacturaDto v_factura=null;
+	TicUserDto v_Usuario=null;
+	GeneralSrv v_client=null;
+	BigDecimal saldo_cuenta=new BigDecimal(0);
+    private BigDecimal saldo_factura;
+    
+    private BeanFactory path;
+
+    @Given("pagar Servicios en Linea <p_usuario>")
+    public void pagarServicios(@Named("p_usuario") String p_usuario) {
+    	
+    	v_client = new GeneralSrvImpl();
+    	
+    	if(v_client!=null){
+    		v_Usuario=v_client.findUsuario(p_usuario);
+    		
+    	}
+    }
+    
+    @When("selecciono la Cuenta asociada a mi usuario <p_cuenta>")
+    public void SeleccionoCuenta(@Named("p_cuenta") String p_cuenta) {
+    	if(v_Usuario!=null){
+    		v_Cuenta=new TicCuentaDto();
+    		v_Cuenta.setNo_Cuenta(p_cuenta);
+    		v_Usuario.setListaCuentas(new ArrayList<TicCuentaDto>());
+    		v_Usuario.getListaCuentas().add(v_Cuenta);
+    	}
+    }
+
+    @When("ingreso el numero de factura <p_no_factura>")
+    public void ingresarNoFactura(@Named("p_no_factura") String  p_no_factura) {
+    	if(v_client!=null){
+    		v_factura  =v_client.findFactura(p_no_factura);
+    		
+    	}
+    }
+
+    @When("ingreso el monto a pagar <p_monto>")
+    public void validarMonto(@Named("p_monto") String p_monto) {
+    	try{
+    		saldo_factura=new BigDecimal(p_monto);
+			 
+		}catch(Exception e){
+			 
+		}
+    }
+    
+    @When("presiono el boton de Transmitir")
+    public void transmitiendo() {
+    	if(v_Usuario!=null ){
+    		if(v_client!=null && v_Usuario.getListaCuentas().size()>0){
+    			saldo_cuenta = v_client.findSaldoCuenta(v_Usuario.getListaCuentas().get(0).getNo_Cuenta());
+    		}
+    	}
+    }
+    
+    @Then("la factura existe")
+    public void checkFactura() {
+    		if(v_factura == null){
+    			assertFalse(true);
+    		}else{
+    			assertTrue(true);
+    		}
+    	    
+    }
+    
+    @Then("el monto no corresponde a la factura")
+    public void checkSaldoFactura() {
+    	   if(saldo_factura!=null && v_factura!=null  ){
+    		   if(v_factura.getValor().compareTo(saldo_factura)==0){
+    			   assertTrue(true);
+    			  
+    			   
+    		   }else{
+    			   assertFalse(true);
+    		   }
+    		   
+    	   }else{
+    		   assertFalse(true);
+    	   }
+    }
+    
+    @Then("la cuenta no tiene fondos para realizar el pago")
+    public void checkSaldoCuenta() {
+    	   if(saldo_cuenta!=null && saldo_factura!=null){
+    		   		if(saldo_cuenta.compareTo(saldo_factura)>=0){
+    				   assertTrue(true);
+    			   } else{
+    				   assertFalse(true);
+    			   }
+    		   
+    	   }else{
+    		   assertFalse(true);
+    	   }
+    }
+    
+    @Then("se despliega el comprobante de pago")
+    public void checkUsuario() {
+    	    assertNotNull(v_Usuario);
+    }
+    
+
+
+}
---
+++ trunk/primer_proyecto/matic-as02/src/test/java/gt/usac/ing/tic/b_a_c_scenario.story
@@ -0,0 +1,29 @@
+Narrative: Titular de la Cuenta paga un factura a partir banca
+    Como titular de la cuenta
+    Quiere pagar con su cuenta en la banca
+    Con el fin de obtener dinero cuando el banco esta cerrado
+
+Scenario: paga un factura a partir banca
+Given pagar Servicios en Linea <p_usuario>
+When selecciono la Cuenta asociada a mi usuario <p_cuenta>
+When ingreso el numero de factura <p_no_factura>
+When ingreso el monto a pagar <p_monto>
+When presiono el boton de Transmitir
+Then la factura existe
+Then el monto no corresponde a la factura
+Then la cuenta no tiene fondos para realizar el pago
+Then se despliega el comprobante de pago
+
+Examples:
+|p_usuario|p_cuenta|p_no_factura|p_monto|
+|lucita287@gmail.com|33230266888|FACT0003|900|
+|lucita287@gmail.com|33130266522|FACT0001|600|
+|yajomac@gmail.com|44330216528|FACT0002|700|
+|yajomac@gmail.com|34130216527|FACT0004|1800|
+|prisila.flores@gmail.com|31330819219|FACT0006|180|
+|prisila.flores@gmail.com|44330815921|FACT0007|80|
+
+
+
+
+
